525571971edb38e6da67c09c523ef8da4bae8ee2
PavaneeshwarReddy <pavaneesshwar7077@gmail.com>
2023-08-14 20:35:44 +0530

    first commit

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './footer.scss';
+
+
+
+const Footer = () => {
+
+    return (
+        <div>
+            {/* <div className="footer">
+                <div>
+                    <img className="logo" onClick={() => navigate('/')} src={LogoMsn} alt="logo" />
+                </div>
+                <div className="social-wrapper">
+                    <Icon img={ICFacebook} />
+                    <Icon img={ICTwitter} />
+                    <Icon img={ICInstagram} />
+                    <Icon img={ICTelegram} />
+                    <Icon img={ICDiscord} />
+                    <Icon img={ICGithub} />
+                </div>
+            </div>
+            <div className="copyright">
+                <p>© 2023 Gilbert Hutapea. All Rights Reserved</p>
+            </div> */}
+        </div>
+    )
+}
+
+export default Footer;
